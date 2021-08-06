6b9cfa6a58491ef189180f11c68c1cfcd4218073
Ethan Degenhardt <ethandeg1996@gmail.com>
2021-08-05 22:06:37 -0500

    understanding the basics of vue

---
+++ data.js
@@ -0,0 +1,22 @@
+const cards = [
+    {
+    front: "hello",
+    back: "world"
+    },
+    {
+    front: "blarch",
+    back: "metndlskd"
+    },
+    {
+    front: "roman",
+    back: "reigns"
+    },
+    {
+    front: "jim",
+    back: "nugget"
+    },
+    {
+    front: "what is",
+    back: "your deal"
+    }
+]
